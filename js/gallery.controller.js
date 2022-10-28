dfd56ca4aafab801992d420e1c217ee63fb8cf23
TalLiber <114108637+TalLiber@users.noreply.github.com>
2022-10-28 15:37:08 +0300

    Add files via upload

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -3,6 +3,23 @@
 //TODO remove on MyMemes
 function onInit() {
     renderImgs()
+
+    const shareData = {
+        title: 'MDN',
+        text: 'Learn web development on MDN!',
+        url: 'https://developer.mozilla.org'
+    }
+
+    const btn = document.querySelector('.btnn');
+    const resultPara = document.querySelector('.result');
+    btn.addEventListener('click', async() => {
+        try {
+            await navigator.share(shareData);
+            resultPara.textContent = 'MDN shared successfully';
+        } catch (err) {
+            resultPara.textContent = `Error: ${err}`;
+        }
+    });
 }
 
 function renderImgs() {
